0db3ba7ef4b84ef099686eb71ab1170a5b9f7825
Deathworld <corentin.grall@gmail.com>
2018-06-07 17:20:34 +0000

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -88,7 +89,6 @@ app.get('/bad', (req, rep) => {
    });
 });
 
-var port = 3000;
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
